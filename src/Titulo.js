768af113496d0e8ab85f3f70e8dc636a783ce70d
Daniel Ramírez <nassao@gmail.com>
2023-08-28 19:46:28 -0500

    Adiciond e Titulo

---
+++ src/Titulo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Titulo() {
+	return (
+		<>
+			<h1 id="titulo-tabla" data-testid="h1-titulo-tabla">
+				Tabla de postres
+			</h1>
+			<h2>Los mas ricos</h2>
+		</>
+)
+}

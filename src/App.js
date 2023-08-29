4b83a1083fea503e01dfc645c111ecbead5407ec
Daniel Ramírez <4997958+nassao@users.noreply.github.com>
2023-08-28 19:16:27 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,66 @@
-// Todo
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Torta', 200, 16.0, 49, 3.9),
+  createData('Brazo de Reina', 400, 16.0, 49, 3.9),
+  createData('Brownie', 123, 16.0, 49, 3.9),
+<<<<<<< HEAD
+<<<<<<< HEAD
+  createData('Merengon', 1000, 20.0, 100, 0.1),
+=======
+  createData('Basbousa', 123, 16.0, 49, 3.9)
+>>>>>>> 9845699 (Nuevo Postre Basbousa)
+=======
+  createData('IslasFlotantes', 150, 15.0, 50, 3.9),
+>>>>>>> 43c271c (Postre con merengue)
+];
+
+export default function BasicTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}

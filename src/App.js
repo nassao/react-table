d65730f2d6bfafd6ada2ff2ffd5d3b961a0d93ba
Daniel Ramírez <nassao@gmail.com>
2023-08-28 19:25:37 -0500

    Corrección

--- src/App.js
+++ src/App.js
@@ -6,61 +6,93 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Titulo from './Titulo';
 
-function createData(name, calories, fat, carbs, protein) {
+// Seccion de postres
+
+/**
+ * Crea un nuevo postre
+ * @param {*} name 
+ * @param {*} calories 
+ * @param {*} fat 
+ * @param {*} carbs 
+ * @param {*} protein 
+ * @returns 
+ */
+function crdt(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Torta', 200, 16.0, 49, 3.9),
-  createData('Brazo de Reina', 400, 16.0, 49, 3.9),
-  createData('Brownie', 123, 16.0, 49, 3.9),
-<<<<<<< HEAD
-<<<<<<< HEAD
-  createData('Merengon', 1000, 20.0, 100, 0.1),
-=======
-  createData('Basbousa', 123, 16.0, 49, 3.9)
->>>>>>> 9845699 (Nuevo Postre Basbousa)
-=======
-  createData('IslasFlotantes', 150, 15.0, 50, 3.9),
->>>>>>> 43c271c (Postre con merengue)
+
+// Seccion de numeros
+export const getRandom = () => Math.random() * 100;
+export const getSuma = (a, b) => a+b;
+export const modulo = (num, base) => num % base;
+
+/**
+ * 
+ * @returns {integer} 
+ */
+export const rdm1a20 = () => Math.ceil(Math.random() * 20);
+
+// Seccion de postres
+
+/**
+ * Lista de postres
+ */
+const r = [
+  crdt('Chocolatina Hersheys', 180, 17.0, 40, 2.5),
+  crdt('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  crdt('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  crdt('Eclair', 262, 16.0, 24, 6.0),
+  crdt('Cupcake', 305, 3.7, 67, 4.3),
+  crdt('Gingerbread', 356, 16.0, 49, 3.9),
+  crdt('Torta', 200, 16.0, 49, 3.9),
+  crdt('Brazo de Reina', 400, 16.0, 49, 3.9),
+  crdt('Brownie', 123, 16.0, 49, 3.9),
+  crdt('Merengon', 400, 20.0, 100, 0.1),
+  crdt('Tiramisu', 518, 18.0, 50, 4.5),
+  crdt('Basbousa', 123, 16.0, 49, 3.9),
+  crdt('IslasFlotantes', 150, 15.0, 50, 3.9),
+  crdt('Manjar Blanco', 160, 16.0, 70, 5.6),
+  crdt('Cakepop', 10, 16.0, 10, 5.6),
+  crdt('Salami', 10, 16.0, 10, 5.6),
+  crdt('Arequipe', 120, 15.7, 23, 8.2),
 ];
 
 export default function BasicTable() {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+    <>
+      <Titulo/>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {r.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,102 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App, { getRandom, getSuma, modulo } from './App';
 
-test('renders learn react link', () => {
+test('Se renderiza la App', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  expect(screen).toBeDefined();
+});
+
+test('Se renderiza el título', () => {
+  render(<App />);
+  const linkElement = screen.getByText("Tabla de postres");
   expect(linkElement).toBeInTheDocument();
 });
+
+test('El título tiene ID', () => {
+  render(<App />);
+  // eslint-disable-next-line testing-library/no-node-access
+  const linkElement = document.querySelector("#titulo-tabla");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('El título tiene TestID', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId("h1-titulo-tabla");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Un postre es el Arequipe',() => {
+  render(<App/>);
+  const linkElement = screen.getAllByText("Arequipe");
+  expect(linkElement).toBeDefined();
+});
+
+// Matemáticas
+
+test('Randomizador retorna un número',() => {
+  const random = getRandom();
+  expect(random).toBeGreaterThanOrEqual(0);
+});
+
+test('Randomizador retorna un aleatorio',() => {
+  const random1 = getRandom();
+  const random2 = getRandom();
+  expect(random1 === random2).toBeFalsy();
+});
+
+test('Se suman 2 numeros positivos',() => {
+  const numero1 = 1;
+  const numero2 = 1;
+  expect((numero1+numero2) === getSuma(numero1,numero2)).toBeTruthy();
+});
+
+test('Se suman 2 numeros negativos',() => {
+  const numero1 = -1;
+  const numero2 = -1;
+  expect((numero1+numero2) === getSuma(numero1,numero2)).toBeTruthy();
+});
+
+test('Se suman un numero negativo y un numero positivo',() => {
+  const numero1 = 1;
+  const numero2 = -1;
+  expect((numero1+numero2) === getSuma(numero1,numero2)).toBeTruthy();
+});
+
+test('Se suman 2 numeros ceros',() => {
+  const numero1 = 0;
+  const numero2 = 0;
+  expect((numero1+numero2) === getSuma(numero1,numero2)).toBeTruthy();
+});
+
+
+test('3 en módulo 2 es 1', ()=>{
+  const numero = 3;
+  const base = 2;
+  expect(modulo(numero,base) === 1).toBeTruthy();
+});
+
+test('2 en módulo 2 es 0', ()=>{
+  const numero = 2;
+  const base = 2;
+  expect(modulo(numero,base) === 0).toBeTruthy();
+});
+
+test('1 en módulo 2 es 1', ()=>{
+  const numero = 1;
+  const base = 2;
+  expect(modulo(numero,base) === 1).toBeTruthy();
+});
+
+test('5 en módulo 2 es 1', ()=>{
+  const numero = 5;
+  const base = 2;
+  expect(modulo(numero,base) === 1).toBeTruthy();
+});
+
+test('6 en módulo 2 es 0', ()=>{
+  const numero = 6;
+  const base = 2;
+  expect(modulo(numero,base) === 0).toBeTruthy();
+});
+
+// Unit Test para numeros aleatorios del 1 al 20
